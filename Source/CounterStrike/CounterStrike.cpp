93f6429163d6e63f8a109b6b0a3d5210126d643c
Shin Jun Cheol <904sinjun@gmail.com>
2021-03-31 19:53:36 +0900

    delete

--- Source/CounterStrike/CounterStrike.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CounterStrike.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CounterStrike, "CounterStrike" );
